899bc54e9c700d65ba14162d7cac6ec172aecaec
Yogesh Bharate <yogesh.bharate@mastercard.com>
2020-01-06 16:05:18 +0530

    Added methods regarding the sending the network request

--- SDWebImage/NetworkManager/NetworkManager.swift
+++ SDWebImage/NetworkManager/NetworkManager.swift
@@ -8,7 +8,31 @@
 import Foundation
 
 public class NetworkManager {
-  public static func printResult() {
-    print("Result")
+  public func sendRequest(urlPath: String, parameters: [String: Any], httpMethod: String = "POST",  completionHandler: @escaping(_ response: Any?, _ error: Error?) -> Void) {
+    
+    let url = URL(string: urlPath)
+    
+    var request = URLRequest(url: url!)
+    request.addValue(WalletConstants.applicationJSON, forHTTPHeaderField: WalletConstants.contentType)
+    request.httpMethod = httpMethod
+    do {
+      request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+    } catch let error {
+      completionHandler(nil, error)
+    }
+    
+    URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
+      do {
+        if let data = data,
+          let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? NSDictionary {
+          print("JSON ---- > \(json)")
+          completionHandler(json, nil)
+        } else {
+          completionHandler(nil, error)
+        }
+      } catch let error {
+        completionHandler(nil, error)
+      }
+    }).resume()
   }
 }
