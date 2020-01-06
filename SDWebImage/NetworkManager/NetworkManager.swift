c46071fa1adce600d69414ed51fce4b656f8be92
Yogesh Bharate <yogesh.bharate@mastercard.com>
2020-01-06 12:42:22 +0530

    Multiple Library

---
+++ SDWebImage/NetworkManager/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yogesh Bharate on 1/3/20.
+//
+
+import Foundation
+
+public class NetworkManager {
+  public static func printResult() {
+    print("Result")
+  }
+}
